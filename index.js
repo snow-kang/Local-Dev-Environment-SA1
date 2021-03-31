ba8467025641bcc49ddd8c267af28d87effa4403
Snow Kang <snow.kang.21@dartmouth.edu>
2021-03-31 16:49:20 -0400

    Code cleanup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+const MAX_SNOWFLAKES = 500;
 
 /* TEXT LOCAL VARIABLES */
 let idx = 0;
@@ -10,7 +11,7 @@ let speed = 100; // speed of the typing effect (duration in milliseconds)
 /* SCENE LOCAL VARIABLES */
 let w = (canvas.width = window.innerWidth);
 let h = (canvas.height = window.innerHeight);
-let particlesArray = [];
+let snowflakesArray = [];
 
 /* TEXT FUNCTIONS */
 function sleep(ms) {
@@ -36,16 +37,16 @@ function random(min, max) {
   return min + Math.random() * (max - min);
 }
 
-// Adjusts for window resizing
 function clientResize(env) {
   w = canvas.width = window.innerWidth;
   h = canvas.height = window.innerHeight;
+  drawSnowFlakes();
 }
 window.addEventListener("resize", clientResize);
 
 // Upon mouse click, generates a snowflake
 function clientMousePress(env) {
-  particlesArray.push({
+  snowflakesArray.push({
     x: env.x,
     y: env.y,
     opacity: random(0.5, 1),
@@ -58,30 +59,35 @@ window.addEventListener("click", clientMousePress);
 
 // Snowfall: Followed tutorial from https://www.youtube.com/watch?v=50teKYVaQgc for the functions below
 function drawSnowFlakes() {
-  for (var i = 0; i < particlesArray.length; i++) {
+  for (var i = 0; i < snowflakesArray.length; i++) {
     // Draws a circle
     ctx.beginPath();
     ctx.arc(
-      particlesArray[i].x, // x-coord
-      particlesArray[i].y, // y-coord
-      particlesArray[i].radius, // radius
+      snowflakesArray[i].x, // x-coord
+      snowflakesArray[i].y, // y-coord
+      snowflakesArray[i].radius, // radius
       0, // start angle (rad)
       Math.PI * 2 // end angle (rad)
     );
-    ctx.fillStyle = `rgba(255, 255, 255, ${particlesArray[i].opacity})`;
+    ctx.fillStyle = `rgba(255, 255, 255, ${snowflakesArray[i].opacity})`;
     ctx.fill();
   }
 }
 
 function moveSnowFlakes() {
-  for (var i = 0; i < particlesArray.length; i++) {
-    particlesArray[i].x += particlesArray[i].speedX;
-    particlesArray[i].y += particlesArray[i].speedY;
+  for (var i = 0; i < snowflakesArray.length; i++) {
+    snowflakesArray[i].x += snowflakesArray[i].speedX;
+    snowflakesArray[i].y += snowflakesArray[i].speedY;
 
     // After a snowflake falls out of view -> move it back to the top of the window
-    if (particlesArray[i].y > h) {
-      particlesArray[i].x = random(-10, w + 10);
-      particlesArray[i].y = -30;
+    if (snowflakesArray[i].y > h) {
+      // Start removing snowflakes if there are already >= MAX_SNOWFLAKES snowflakes on the screen
+      if (snowflakesArray.length < MAX_SNOWFLAKES) {
+        snowflakesArray[i].x = random(-10, w + 10);
+        snowflakesArray[i].y = -30;
+      } else {
+        snowflakesArray.splice(i, 1);
+      }
     }
   }
 }
